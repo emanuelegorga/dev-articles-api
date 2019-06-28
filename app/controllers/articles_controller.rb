36207c141325491323090a3c9e7847cab39d2376
Emanuele Gorga <emanuelegorga@gmail.com>
2019-06-28 07:47:30 +0100

    Implemented show action in Article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
-    def index
-        render json: 'Test index action'
-    end
+	def index
+		articles = Article.all
+		render json: articles
+	end
+	
+	def show
+	end
 end
--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -4,4 +4,8 @@ describe 'articles routes' do
 	it 'should root to articles index' do
 		expect(get '/articles').to route_to('articles#index')
 	end
+
+	it 'should root to articles show' do
+		expect(get '/articles/1').to route_to('articles#show', id: '1')
+	end
 end
