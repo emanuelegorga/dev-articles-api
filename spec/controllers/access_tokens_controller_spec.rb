4f1f3e7e01dd46d5f809d3b16f4709239467dd86
Emanuele Gorga <emanuelegorga@gmail.com>
2019-07-14 19:26:06 +0100

    Refactoring error tests

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,27 +2,6 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
 	describe '#create' do
-		shared_examples_for "unauthorized_requests" do
-			let(:authentication_error) do
-				{
-					"status" => "401",
-					"source" => { "pointer" => "/code" },
-					"title" =>  "Authentication code is invalid",
-					"detail" => "You must provide valid code in order to exchange it for token."
-				}
-			end
-
-			it 'should return 401 status code' do
-				subject
-				expect(response).to have_http_status(401)
-			end
-
-			it 'should return proper error body' do
-				subject
-				expect(json['errors']).to include(authentication_error)
-			end
-		end
-
 		context 'when no code is provided' do
 			subject { post :create }
 			it_behaves_like 'unauthorized_requests'
@@ -77,27 +56,17 @@ RSpec.describe AccessTokensController, type: :controller do
   end
   
   describe 'DELETE #destroy' do
-    context 'when invalid request' do
-      let(:authorization_error) do
-				{
-					"status" => "403",
-					"source" => { "pointer" => "/headers/authorization" },
-					"title" =>  "Not authorized",
-					"detail" => "You have no rights to access this resource."
-				}
-      end
-      
-      subject { delete :destroy }
+    subject { delete :destroy }
 
-      it 'should return 403 status code' do
-        subject
-        expect(response).to have_http_status(:forbidden)
-      end
+    context 'when no authorization header provided' do
 
-      it 'should return proper error json' do
-        subject
-        expect(json['errors']).to include(authorization_error)
-      end
+      it_behaves_like 'forbidden_requests'
+    end
+
+    context 'when invalid authorization header provided' do
+      before { request.headers['authorization'] = 'Invalid token' }
+
+      it_behaves_like 'forbidden_requests'
     end
 
     context 'when valid request' do
