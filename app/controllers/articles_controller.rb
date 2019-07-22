0c6c6aaa947702249b6b3f89da115fdfe9598a52
Emanuele Gorga <emanuelegorga@gmail.com>
2019-07-22 16:46:03 +0100

    Added #destroy action to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,14 @@ class ArticlesController < ApplicationController
       status: :unprocessable_entity
   end
 
+  def destroy
+    article = current_user.articles.find(params[:id])
+    article.destroy
+    head :no_content
+  rescue
+    authorization_error
+  end
+
 	private
 
 	def serializer
