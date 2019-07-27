dd0e3cf6eaaae50745f46698782f906bf9e8c137
Emanuele Gorga <emanuelegorga@gmail.com>
2019-07-27 15:47:49 +0100

    Added User database seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+user = User.create(
+  { login: 'emanuele', name: 'Emanuele Dev', provider: 'github' }
+)
+
 Article.create([
-    { title: 'Article title 1', content: 'Article content 1', slug: 'Article slug 1' },
-    { title: 'Article title 2', content: 'Article content 2', slug: 'Article slug 2' },
-    { title: 'Article title 3', content: 'Article content 3', slug: 'Article slug 3' }
+    { title: 'Article title 1', content: 'Article content 1', slug: 'article-slug-1', user: user },
+    { title: 'Article title 2', content: 'Article content 2', slug: 'article-slug-2', user: user },
+    { title: 'Article title 3', content: 'Article content 3', slug: 'article-slug-3', user: user }
 ])
