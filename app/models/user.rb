7bfd1e4729e91b435952b3101695113213c7c7ac
Emanuele Gorga <emanuelegorga@gmail.com>
2019-07-29 22:30:46 +0100

    Added RegistrationsController. Added sign_up endpoint in order to register users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,22 @@ class User < ApplicationRecord
   include BCrypt
     validates :login, presence: true, uniqueness: true
     validates :provider, presence: true
+    validates :password, presence: true, if: :standard_provider?
 
     has_one :access_token, dependent: :destroy
     has_many :articles, dependent: :destroy
     has_many :comments, dependent: :destroy
+
+    def standard_provider?
+      provider == 'standard'
+    end
 
     def password
       @password ||= Password.new(encrypted_password) if encrypted_password.present?
     end
 
     def password=(new_password)
+      return @password = new_password if new_password.blank?
       @password = Password.create(new_password)
       self.encrypted_password = @password
     end
